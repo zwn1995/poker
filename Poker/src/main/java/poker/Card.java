d8782c5fec8e5a4c1efca1e091175a991fdc3752
zwn1995 <49931679+zwn1995@users.noreply.github.com>
2019-05-12 22:17:27 +0800

    Update Card.java

--- Poker/src/main/java/poker/Card.java
+++ Poker/src/main/java/poker/Card.java
@@ -83,7 +83,7 @@ public class Card implements Comparable<Card> {
     }
 
     @Override
-    public int compareTo(Card otherCard) {
+    public int compareTo(Card otherCard) { //比较大小
         return this.value.compareTo(otherCard.value);
     }
 
